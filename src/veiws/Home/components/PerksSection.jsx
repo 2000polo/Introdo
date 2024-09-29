412d54db12af1b3e43c286770a35ab0e0fece7a1
ArunPaul <frontendteam@oceanix.cloud>
2024-09-29 11:29:51 +0530

    grid responsiveness added

--- src/veiws/Home/components/PerksSection.jsx
+++ src/veiws/Home/components/PerksSection.jsx
@@ -9,14 +9,14 @@ import { IoChevronUp } from 'react-icons/io5';
 
 const PerksSection = () => {
   return (
-    <div className='grid grid-cols-12 justify-items-center px-1 md:px-24 lg:px-64 pt-20 gap-6'>
+    <div className='grid grid-cols-12 justify-items-center px-3 md:px-24 lg:px-64 pt-20 gap-6'>
         <span class=" col-span-12 w-fit rounded-full py-2 px-6 text-[#FC1777] text-semibold md:text-lg font-medium me-2 rounded border-2 border-[#FC1777]">WHY INTRODO?</span>
         <h1 className=' col-span-12 text-4xl md:text-6xl font-bold w-fit text-secondary'>Unlock The Perks!</h1>
 
         <div className="col-span-12 ">
-                <div className="my-10 h-full grid gap-8 sm:mt-16 lg:grid-cols-3 lg:grid-rows-3">
+                <div className="my-10 h-full grid gap-8 sm:mt-16 grid-cols-3 grid-rows-3">
 
-                    <div className=" lg:row-span-3">
+                    <div className="col-span-3 lg:col-span-1 lg:row-span-3">
                         {/* <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div> */}
 
                         <div className=" text-white bg-gradient-to-br from-amber-400 to-pink-500 p-8 flex h-full flex-col rounded-3xl">
@@ -38,7 +38,7 @@ const PerksSection = () => {
 
                     </div>
 
-                    <div className="flex gap-8 flex-col justify-between lg:row-span-3 row-start-1 col-start-2 h-full  ">
+                    <div className="col-span-3 lg:col-span-1 flex gap-8 flex-col justify-between lg:row-span-3 lg:row-start-1 lg:col-start-2 h-full  ">
                         <div className="h-full bg-gradient-to-b from-[#FD4190] to-[#A8C5FF] rounded-3xl  text-white">
                             {/* <div className="absolute inset-px rounded-lg max-lg:rounded-t-[2rem]"></div> */}
                             <div className="p-8 relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
@@ -91,7 +91,7 @@ const PerksSection = () => {
                         </div>
                     </div>
 
-                    <div className="relative col-start-3 row-start-1 row-span-3 overflow-hidden ">
+                    <div className="col-span-3 lg:col-span-1 relative lg:col-start-3 lg:row-start-1 lg:row-span-3 overflow-hidden ">
                         <div className=" overflow-hidden text-white bg-gradient-to-br from-[#69489A] to-[#5E9CFF] p-8 flex h-full flex-col rounded-3xl">
                             <div className=" pb-3 ">
                                 <p className="mt-2 text-4xl font-semibold tracking-tight text-gray-950 max-lg:text-center">
@@ -111,32 +111,32 @@ const PerksSection = () => {
                     </div>
             </div>
         </div>
-        <div className="flex gap-8 w-full col-span-12 mt-10 pt-8">
-            <div className="text-white relative py-8 px-8 sm:pt-8 flex flex-row rounded-[calc(theme(borderRadius.lg)+1px)] bg-gradient-to-br from-[#FD6BA8] to-[#5E9CFF] rounded-3xl w-full ">
-                {/* <div className=" relative py-8 px-8 sm:pt-8 flex flex-row rounded-[calc(theme(borderRadius.lg)+1px)]"> */}
-                        <div className="w-[65%]">
-                            <p className=" text-4xl font-semibold tracking-tight text-gray-950 max-lg:text-center">Rock-Solid Compliance</p>
-                            <p className="mt-2 max-w-lg font-base text-lg text-sm/6 max-lg:text-center tracking-wide">
-                            Stay GCC law-aligned amidst changing regulations. Worry-free compliance across locations, preventing penalties and legal hassles.
-                            </p>
-                        </div>
-                        <div className="absolute h-full bottom-2 top-2 right-0">
-                            <img
-                                className="h-full size-full object-cover object-top self-end"
-                                src={gridImage4}
-                                alt=""
-                            />
-                        </div>
-                {/* </div> */}
+        <div className="flex flex-col lg:flex-row gap-8 w-full col-span-12 mt-5 lg:mt-10 pt-8">
+            
+            <div className="bg-gradient-to-br from-[#FD6BA8] to-[#5E9CFF] rounded-3xl w-full overflow-hidden ">
+                <div className="h-[450px] md:h-full text-white relative py-8 px-8 sm:pt-8 flex flex-col lg:flex-row flex-row-reverse text-end rounded-[calc(theme(borderRadius.lg)+1px)]">
+                    <div className="lg:w-[65%]">
+                        <p className=" md:text-4xl font-semibold text-xl tracking-tight text-gray-950 max-lg:text-center">Rock-Solid Compliance</p>
+                        <p className="mt-2 max-w-lg text-gray-200 font-base md:text-lg text-sm/6 max-lg:text-center tracking-wide">
+                        Stay GCC law-aligned amidst changing regulations. Worry-free compliance across locations, preventing penalties and legal hassles.                        </p>
+                    </div>
+                    <div className="absolute lg:h-full bottom-0 lg:top-2 right-0">
+                        <img
+                            className="h-[250px] size-full object-fit object-top self-end"
+                            src={gridImage4}
+                            alt=""
+                        />
+                    </div>
+                </div>
             </div>
             <div className="bg-gradient-to-b from-[#8A70B0] to-[#482084] rounded-3xl w-full overflow-hidden ">
-                <div className="h-full text-white relative py-8 px-8 sm:pt-8  flex flex-row flex-row-reverse text-end rounded-[calc(theme(borderRadius.lg)+1px)]">
-                    <div className="w-[65%]">
-                        <p className=" text-4xl font-semibold tracking-tight text-gray-950 max-lg:text-center">Cloud-Powered Savings</p>
-                        <p className="mt-2 max-w-lg font-base text-lg text-sm/6 max-lg:text-center tracking-wide">
+                <div className="h-[350px] md:h-full text-white relative py-8 px-8 sm:pt-8 flex flex-col lg:flex-row flex-row-reverse text-end rounded-[calc(theme(borderRadius.lg)+1px)]">
+                    <div className="lg:w-[65%]">
+                        <p className=" md:text-4xl font-semibold text-xl tracking-tight text-gray-950 max-lg:text-center">Cloud-Powered Savings</p>
+                        <p className="mt-2 max-w-lg text-gray-200 font-base md:text-lg text-sm/6 max-lg:text-center tracking-wide">
                         Centralize people management with one HR software. Save time and cut costs by leveraging cloud technology.                         </p>
                     </div>
-                    <div className="absolute h-full bottom-0 top-1 left-0 ">
+                    <div className="absolute lg:h-full bottom-0 right-0 left-0">
                         <img
                             className="h-full size-full object-cover object-top self-end"
                             src={gridImage5}
